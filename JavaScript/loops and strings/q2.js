6d402408c17a8ae412615d91159ab96043a292bf
pruthvipatel2024 <patelpruthvi671@gmail.com>
2026-01-27 19:00:19 +0530

    loop and string complete

---
+++ JavaScript/loops and strings/q2.js
@@ -0,0 +1,10 @@
+gamenum = 35;
+
+let usernum = prompt("Guess the Game number: ");
+
+while (usernum != gamenum) {
+    usernum = prompt("You guess Wrong Number Please guess Right Number: ");
+}
+
+console.log("Congratulations you guess Right Number...!");
+alert("Congratulations you guess Right Number...!");
---
+++ JavaScript/loops and strings/q3.js
@@ -0,0 +1,4 @@
+let fname = prompt("Enter Your Name to Create A Username:");
+
+let usrname = "@" + fname + 0 + fname.length;
+console.log(usrname);
